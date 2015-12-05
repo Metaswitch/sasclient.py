48963ee7404c7d1a15d9d0119be47bec5c348aed
Chris Coomber <christopher.coomber@metaswitch.com>
2015-12-05 13:03:09 +0000

    Let the calling app control logging

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
-import logging
-import sys
-
 from setuptools import setup, find_packages
-from logging import StreamHandler
-
-# _log = logging.getLogger("sasclient")
-# _log.setLevel(logging.DEBUG)
-# _handler = StreamHandler(sys.stderr)
-# _handler.setLevel(logging.DEBUG)
-# _log.addHandler(_handler)
 
 setup(
     name='metaswitchsasclient',
     version='0.1',
     packages=find_packages('src'),
     package_dir={'':'src'},
-    # test_suite='metaswitch.sasclient.test',
     install_requires=[],
-    # tests_require=[]
     )
--- src/metaswitch/sasclient/__init__.py
+++ src/metaswitch/sasclient/__init__.py
@@ -1,3 +1,11 @@
+import logging
+
 from metaswitch.sasclient.main import Client, Trail
 from metaswitch.sasclient.messages import Event, TrailAssoc, Marker
 from metaswitch.sasclient.constants import *
+
+logger = logging.getLogger(__name__)
+# Let the calling application control logging
+logger.addHandler(logging.NullHandler())
+# Some of our applications set logging on the root logger. Don't inherit from that by default.
+logger.propagate = False
